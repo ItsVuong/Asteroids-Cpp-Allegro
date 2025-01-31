2d153866e577df708ccc63ee4aaf1e64f42cdd91
ItsVuong <itsvuong0806@gmail.com>
2025-01-31 18:43:19 +0700

    Update bullet logic

--- bullet.cpp
+++ bullet.cpp
@@ -14,26 +14,39 @@ void fireBullet(Bullet bullets[], int size, Ship ship) {
       bullets[i].angle = ship.angle;
       bullets[i].coorX = ship.coorX;
       bullets[i].coorY = ship.coorY;
+      bullets[i].velocityX = ship.velocityX;
+      bullets[i].velocityY = ship.velocityY;
       break;
     }
   }
 }
 
-void drawBullet(Bullet bullets[], int size) {
+void drawBullet(Bullet bullets[], int size, int screendWidth, int screenHeight) {
   for (int i = 0; i < size; i++) {
     if (bullets[i].isAlive) {
-      al_draw_circle(bullets[i].coorX + 500 +2* cos(bullets[i].angle * (M_PI / 180.0)), 
-                     bullets[i].coorY + 350 -2* sin(bullets[i].angle * (M_PI / 180.0)), 1,
-                     al_map_rgb(255, 255, 255), 2);
+      al_draw_circle(
+          bullets[i].coorX + screendWidth/2.0,
+          bullets[i].coorY + screenHeight/2.0,
+          1, al_map_rgb(255, 255, 255), 2);
     }
   }
 }
 
-void updateBullets(Bullet bullets[], int size){
+void updateBullets(Bullet bullets[], int size) {
   for (int i = 0; i < size; i++) {
-    if(bullets[i].isAlive){
-      bullets[i].coorX += bullets[i].speed * cos(bullets[i].angle * (M_PI / 180.0));
-      bullets[i].coorY -= bullets[i].speed * sin(bullets[i].angle * (M_PI / 180.0));
+    if (bullets[i].isAlive) {
+      //The foward velocity of the bullet + the velocity of the ship at the time the bullet is fired
+      bullets[i].coorX +=
+          bullets[i].speed * cos(bullets[i].angle * (M_PI / 180.0)) + bullets[i].velocityX;
+      bullets[i].coorY -=
+          bullets[i].speed * sin(bullets[i].angle * (M_PI / 180.0)) + bullets[i].velocityY;
+      //Increase the time that the bullet has lived each iteration
+      bullets[i].lifeTime++;
+      
+      if (bullets[i].lifeTime > BULLET_LIFE_TIME) {
+        bullets[i].isAlive = false;
+        bullets[i].lifeTime = 0;
+      }
     }
   }
 }
--- bullet.h
+++ bullet.h
@@ -6,17 +6,24 @@
 #include <allegro5/color.h>
 #include <cmath>
 
-#define BULLET_SPEED 16.0;
+#define BULLET_SPEED 16.0
+#define BULLET_LIFE_TIME 60
 struct Bullet{
   double coorX;
   double coorY;
   bool isAlive;
   double speed;
   double angle;
+  int lifeTime;
+
+  //Velocity of the ship at the time a the bullet is fired
+  double velocityX;
+  double velocityY;
 };
 
 void createBullets(Bullet bullets[], int size); 
 void fireBullet(Bullet bullets[], int size, Ship ship);
-void drawBullet(Bullet bullets[], int size);
+void drawBullet(Bullet bullets[], int size, int screendWidth, int screenHeight);
 void updateBullets(Bullet bullets[], int size);
+
 #endif
